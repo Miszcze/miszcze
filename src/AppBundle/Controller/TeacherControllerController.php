fd30b73f0fc2142a60b7f0922253974cf43549fc
unknown <gorskimarcin96@gmail.com>
2018-10-20 21:09:38 +0200

    poprawki u Czerwińskiego

--- src/AppBundle/Controller/TeacherControllerController.php
+++ src/AppBundle/Controller/TeacherControllerController.php
@@ -2,14 +2,18 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Klasy;
+use AppBundle\Entity\GodzLek;
 use AppBundle\Entity\Obecnosci;
 use AppBundle\Entity\Oceny;
+use AppBundle\Entity\Pracownicy;
+use AppBundle\Entity\Przedmioty;
 use AppBundle\Entity\Terminarz;
 use AppBundle\Entity\Uczniowie;
+use AppBundle\Entity\Uwagi;
 use AppBundle\Entity\Zajecia;
 use AppBundle\Form\PresenceType;
 use AppBundle\Form\RatingType;
+use AppBundle\Form\SchoolNoteType;
 use AppBundle\Utils\Message;
 use DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -22,7 +26,7 @@ use Symfony\Component\HttpFoundation\Request;
 class TeacherControllerController extends Controller{
     
     /**
-     * @Route("/obecnosci/{term}", name="presence", defaults={"term"="0"})
+     * @Route("/obecnosci/{term}", name="teacher_presence", defaults={"term"="0"})
      */
     public function presenceAction(Request $request,$term){
 	(new Message($this))->count($this);
@@ -79,7 +83,7 @@ class TeacherControllerController extends Controller{
     }
     
     /**
-     * @Route("/oceny/{term}/{student}", name="rating", defaults={"term"="0","student"="0"})
+     * @Route("/oceny/{term}/{student}", name="teacher_rating", defaults={"term"="0","student"="0"})
      */
     public function ratingAction(Request $request,$term,$student){
 	$em=$this->getDoctrine()->getManager();
@@ -115,5 +119,66 @@ class TeacherControllerController extends Controller{
 	    'students'=>@$students,
 	    'form'=>$form->createView()
 	]);
+    }
+    
+    
+    /**
+     * @Route("/plan-zajec", name="teacher_time_table")
+     */
+    public function timeTableAction(){
+	$em=$this->getDoctrine()->getManager();
+	
+	$lessonHours=$em->getRepository(GodzLek::class)->findAll();
+	$teacher=$em->getRepository(Pracownicy::class)->findOneBy(['uzytkownik'=>$this->get('session')->get('user')['user']]);
+	$subject=$em->getRepository(Przedmioty::class)->findOneBy(['prowadzacy'=>$teacher]);
+	//$timeTable=$em->getRepository(Terminarz::class)->findBy(['ktoCo'=>$subject]);
+	
+	$arrayWeek=['poniedzialek','wtorek','sroda','czwartek','piatek','sobota'];
+	for($j=0;$j<count($arrayWeek);$j++)
+	for($i=0;$i<count($lessonHours);$i++)
+	$lessons[$i][$arrayWeek[$j]]=$em
+	    ->getRepository(Terminarz::class)
+	    ->findOneBy([
+		'dzienTygodnia'=>$arrayWeek[$j],
+		'godzina'=>$lessonHours[$i],
+		'ktoCo'=>$subject
+	    ]);
+	
+	return $this->render('teacher/time_table.html.twig',[
+	    'lessonHours'=>$lessonHours,
+	    'lessons'=>$lessons
+	]);
+    }
+    
+    
+    /**
+     * @Route("/uwagi/{term}/{student}", name="teacher_school_note", defaults={"term"="0","student"="0"})
+     */
+    public function schoolNoteAction(Request $request,$term,$student){
+	$em=$this->getDoctrine()->getManager();
+	
+	$terms=$em->getRepository(Terminarz::class)->findAll();
+	
+	if(!empty($term)){
+	    $form=$this->createForm(SchoolNoteType::class,null,['id'=>$term]);
+	    if($request->getMethod('post')){
+		
+		
+		$schoolNote=new Uwagi();
+		$schoolNote->getUczen();
+		$schoolNote->setOdczytane(0);
+		$schoolNote->setStatus(0);
+	    }
+	}
+	
+	if(!isset($form))
+	    return $this->render('teacher/school_note.html.twig',[
+		'terms'=>$terms
+	    ]);
+	else
+	    return $this->render('teacher/school_note.html.twig',[
+		'terms'=>$terms,
+		'form'=>$form->createView() 
+	    ]);
     }
 }
---
+++ src/AppBundle/Form/SchoolNoteType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Terminarz;
+use AppBundle\Entity\Uczniowie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SchoolNoteType extends AbstractType{
+    
+    public  $em;
+    
+    public function __construct(EntityManagerInterface $em){
+        $this->em=$em;
+    }
+    
+    public function buildForm(FormBuilderInterface $builder,array $options){
+	$em=$this->em;
+	$term=$em->getRepository(Terminarz::class)->find($options['id']);
+	$students=$em->getRepository(Uczniowie::class)->findBy(['klasa'=>$term->getKlasa()]);
+	
+	foreach($students as $value)
+	    $choicesStudent[$value->getNazwisko()." ".$value->getImie()]=$value->getId();
+	
+        $builder->setMethod('POST')
+	    ->add('uczen',ChoiceType::class,['choices'=>$choicesStudent])
+	    ->add('tresc',TextareaType::class)
+	    ->add('submit',SubmitType::class)
+	    ->getForm();
+    }
+    
+    public function configureOptions(OptionsResolver $resolver){
+	$resolver->setDefaults(['id'=>null]);
+    }
+}
