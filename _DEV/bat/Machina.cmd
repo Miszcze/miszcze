35b3094a2e2f84dca35d81240b6a076ee82d528c
dar-glo <dariusz.glowacki@gmail.com>
2018-11-17 12:33:39 +0100

    miszcze

---
+++ _DEV/bat/Machina.cmd
@@ -0,0 +1,38 @@
+CALL :AUTORUN
+cls
+CALL :MACHINA
+
+
+
+GOTO :KONIEC
+
+
+
+
+
+rem ==========================================
+rem ==========================================
+:AUTORUN
+ECHO OFF
+SET COLOR=3F
+SET SCIEZKA=C:\xampp\htdocs\miszcze
+rem ==========================================
+:MACHINA
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO      =======================
+ECHO      =                     =
+ECHO      = M A C H I N A v.1.0 =
+ECHO      =                     =
+ECHO      =======================
+ECHO.
+ECHO.
+ECHO.
+rem ==========================================
+:KONIEC
+ECHO Wcisnij klawisz aby zakonczyc
+pause >nul
+exit
+rem ==========================================
