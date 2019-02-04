14d94e62fa9bccbbbb09cce7abc72f469a9a9a45
Marcin Górski <gorskimarcin96@gmail.com>
2019-02-04 08:54:28 +0100

    nowa baza

---
+++ _DEV/SEMINARIUM.cmd
@@ -0,0 +1,15 @@
+:SEMINARIUM DUCHOWNE
+
+
+DONE add podrozdzial<abstract or summary
+OLANE set %pierwsza_strona%=%wstep% 
+OLANE change rozdzial.align=center na rozdzial.align=left
+min.akapit.lenght=3_line
+DONE wycentrowac podpis tabeli
+DONE poprawić: "na rysunku 3.1" -> "na Rysunku 3.1."
+DONE Przypisy: zostawic tylko RODO
+rem Przy sk0ładaniu pracy
+Potwierdzenie_złożenia_pracy_dyplomowej
+
+apt-get buy Wemos or NodeMCU
+
